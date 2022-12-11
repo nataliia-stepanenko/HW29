4ad042c1f86417145d10d6f7f68875cfbb0ac52b
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-12-12 01:28:55 +0200

    Created Counter component with a basic structure

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+const Counter = () => {
+    return(
+        <>
+        <button>-</button>
+        <input type="number"></input>
+        <button>+</button>
+        </>
+    )
+};
+
+export default Counter;
