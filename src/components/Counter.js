b558f9726cf28f5e53bb37057068384471357a53
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-12-12 23:45:37 +0200

    Made count an input value, added handleInputChange and isDisabled

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,22 @@
 import useCounter from "../hooks/useCounter";
 
+import { useState } from "react";
+
 const Counter = () => {
-    const {count, setCount, increment, decrement, reset,} = useCounter(1);
+    const {count, setCount, increment, decrement,} = useCounter(1);
+    const [isDisabled, setDisabled] = useState(true);
+
+    const handleInputChange = (event) => {
+        setCount(+event.target.value);
+        setDisabled(false);
+    };
 
     return(
         <div>
-            <button onClick={decrement}>-</button>
-            <input type="number" value={count}></input>
+            <button onClick={decrement} disabled={isDisabled}>-</button>
+            <input type="number" onInput={handleInputChange} value={count}></input>
             <button onClick={increment}>+</button>
+            <p>{count}</p>
         </div>
     )
 };
