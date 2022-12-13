1644d9f50557e37e089a293e007b32705dc180b7
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-12-13 23:53:15 +0200

    Installed and imported Chakra, removed useCallback

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,30 @@
-import { memo, useCallback } from "react";
+import { memo } from "react";
+import { Flex } from '@chakra-ui/react';
+import { Button} from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react';
+import {RiAddLine} from 'react-icons/ri';
+import {GrFormSubtract} from 'react-icons/gr';
+
 import useCounter from "../hooks/useCounter";
 
 const Counter = () => {
     const {count, setCount, increment, decrement} = useCounter(1);
 
-    const handleInputChange = useCallback((event) => {
+    const handleInputChange = (event) => {
         setCount(+event.target.value);
         if (+event.target.value < 1){
             setCount(1);
         };
-    }, []);
+    };
 
     const isDisabledButton = (count <= 1);
 
     return(
-        <div>
-            <button onClick={decrement} disabled={isDisabledButton}>-</button>
-            <input type="number" onInput={handleInputChange} value={count}></input>
-            <button onClick={increment}>+</button>
-        </div>
+        <Flex m={4}>
+            <Button px={1} colorScheme='gray' onClick={decrement} disabled={isDisabledButton}><GrFormSubtract size={26} /></Button>
+            <Input mx={2} htmlSize={2} width='auto' value={count} onChange={handleInputChange}></Input>
+            <Button px={1} colorScheme='gray' onClick={increment}><RiAddLine size={26} /></Button>
+        </Flex>
     )
 };
 
