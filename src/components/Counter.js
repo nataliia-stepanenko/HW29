841444e03503b001cc0a0598dc74a9709a5cb490
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-12-13 11:44:31 +0200

    Made decrement button disabled when count =< 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,20 +1,17 @@
 import useCounter from "../hooks/useCounter";
 
-import { useState } from "react";
-
 const Counter = () => {
     const {count, setCount, increment, decrement,} = useCounter(1);
-    const [isDisabled, setDisabled] = useState(true);
 
     const handleInputChange = (event) => {
         setCount(+event.target.value);
-        setDisabled(false);
     };
+    const isDisabledButton = (count <= 1);
 
     return(
         <div>
-            <button onClick={decrement} disabled={isDisabled}>-</button>
-            <input type="number" onInput={handleInputChange} value={count}></input>
+            <button onClick={decrement} disabled={isDisabledButton}>-</button>
+            <input type="number" onInpt={handleInputChange} value={count}></input>
             <button onClick={increment}>+</button>
             <p>{count}</p>
         </div>
